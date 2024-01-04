Fix bug where appropriate powerlaw normalization was not being applied in powerlaw+peak massratio function

gwpop_nearest_neighbor/models/gwpop_models.py
@@ -167,10 +167,14 @@ def PowerlawPlusPeak_MassRatio(data, slope, minimum, delta_m):
     # qs_test = jnp.exp(ln_qs_test)
     # m2s_test = jnp.outer(qs_test, m1s_test)
     dq = qs_test[1] - qs_test[0]
-    power_law_test = powerlaw(qs_test, slope, 0.02, 1.)
+    power_law_test = powerlaw(qs_test, slope, 0.02, 1.) # norm = -jnp.log(jnp.abs(slope + 1)) 
     smoothed_pl_test = power_law_test + m_smoother(m2s_test, minimum, delta_m)
     norm = scs.logsumexp(smoothed_pl_test, axis=0) + jnp.log(dq) # simple Riemann rule
-    norms = norm[jnp.digitize(m1, m1s_test)] # takes the LARGER normalization estimate so to DOWNWEIGHT bad estimates
+
+    # takes the LARGER normalization estimate so to DOWNWEIGHT bad estimates
+    norms = norm[jnp.digitize(m1, m1s_test)] 
+    # correct normalization in the test powerlaw
+    norms += jnp.log(jnp.abs(1 - 0.02**(slope+1))) - jnp.log(jnp.abs(1 - (minimum/m1)**(slope+1)))
     # print(norms, jnp.all(jnp.isfinite(norms)))
     return smoothed_pl - norms
 