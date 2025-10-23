c4bad5a30a894cc1bc9862cb9fffd29e6f412dc0
Jack Heinzel <jack.heinzel@ligo.org>
2025-10-22 23:12:05 -0400

    add url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(name='pixelpop',
         version='0.1',
         description='Meant for use with numpyro and jax, package for fitting the GW population with a nonparameteric binning scheme, where bins are correlated with only their nearest neighbors. Meant for inferring the GW population distribution nonparameterically in higher dimensions.',
-        url='#',
+        url='https://git.ligo.org/jack.heinzel/pixelpop',
         author='Jack Heinzel',
         install_requires=[
             'numpy', 'scipy', 'jax', 'pandas', 'numpyro', 'gwpopulation', 'gwpopulation_pipe', 
