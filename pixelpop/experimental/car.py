9e80f1d97785d4f56dddf54d0d8f07a43d70442a
Sofia Alvarez-Lopez <ms.alvarezl@uniandes.edu.co>
2026-01-27 12:31:46 -0600

    Adding normalized PixelPop working in N-dim (tested in example with Gaussians)

--- pixelpop/experimental/car.py
+++ pixelpop/experimental/car.py
@@ -18,6 +18,7 @@ from functools import reduce
 from ..models.car import initialize_ICAR, add_outer, mult_outer
 
 from numpyro.distributions.transforms import (
+    Transform,
     ComposeTransform,
     StickBreakingTransform, 
     ExpTransform, 
@@ -251,178 +252,196 @@ class DiagonalizedICARTransform:
         return std_lp + 0.5*jnp.sum(jnp.log(self.multiD_eigenvalues))
 
 
-class _LogSimplex(constraints.ParameterFreeConstraint):
+class VectorToGridTransform(Transform):
+    """
+    Reshape (..., n_tot) <-> (..., *event_shape)
+    """
+    domain = constraints.real_vector
+    codomain = constraints.real
     event_dim = 1
-    def __init__(self, logsumexp: ArrayLike) -> None:
-        self.logsumexp = logsumexp
 
-    def __call__(self, x: ArrayLike) -> ArrayLike:
-        x_sum = LSE(x, axis=-1)
-        return (x <= 0).all(axis=-1) & (x_sum < self.logsumexp + 1e-6) & (x_sum > self.logsumexp - 1e-6)
+    def __init__(self, event_shape):
+        super().__init__()
+        self.event_shape = tuple(int(s) for s in event_shape)
+        self.n_tot = int(np.prod(self.event_shape))
 
-    def feasible_like(self, prototype: ArrayLike) -> ArrayLike:
-        return jnp.full_like(prototype, self.logsumexp - jnp.log(prototype.shape[-1]))
+    def __call__(self, x):
+        return jnp.reshape(x, x.shape[:-1] + self.event_shape)
 
-logsimplex = _LogSimplex
-biject_to = numpyro.distributions.transforms.biject_to
+    def _inverse(self, y):
+        return jnp.reshape(y, y.shape[:-len(self.event_shape)] + (self.n_tot,))
 
-@biject_to.register(logsimplex)
-def _transform_to_logsimplex(constraint):
-    return ComposeTransform([
-        StickBreakingTransform(), 
-        ExpTransform().inv, 
-        AffineTransform(constraint.logsumexp, 1.)
-        ])
-
-
-class ICAR_normalized(Distribution):
-    '''
-    
-    TODO IMPLEMENT IN TERMS OF ONE D ADJ MATRICES
-
-    '''
-    arg_constraints = {
-        "log_sigma": constraints.real,
-        "adj_matrix": constraints.dependent(is_discrete=False, event_dim=2),
-        "dx": constraints.positive,
-    }
-    reparametrized_params = [
-        "log_sigma",
-        "adj_matrix",
-        "dx",
-    ]
-
-    pytree_aux_fields = ("is_sparse", "adj_matrix", "dx")
-
-    def __init__(
-        self,
-        log_sigma,
-        adj_matrix,
-        *,
-        is_sparse=False,
-        dx=1., 
-        validate_args=None,
-    ):
-        
-        assert jnp.ndim(log_sigma) == 0
-        self.is_sparse = is_sparse
-        self.dx = dx
-        batch_shape = ()
-        # print('batch shape is ', batch_shape)
-        if self.is_sparse:
-            if adj_matrix.ndim != 2:
-                raise ValueError(
-                    "Currently, we only support 2-dimensional adj_matrix. Please make a feature request",
-                    " if you need higher dimensional adj_matrix.",
-                )
-            if not (isinstance(adj_matrix, np.ndarray) or _is_sparse(adj_matrix)):
-                raise ValueError(
-                    "adj_matrix needs to be a numpy array or a scipy sparse matrix. Please make a feature",
-                    " request if you need to support jax ndarrays.",
-                )
-            self.adj_matrix = adj_matrix
-            # TODO: look into future jax sparse csr functionality and other developments
-        else:
-            assert not _is_sparse(adj_matrix), (
-                "single_dimension_adj_matrix is a sparse matrix so please specify `is_sparse=True`."
-            )
-                # TODO: look into static jax ndarray representation
-            (adj_matrix,) = promote_shapes(
-                adj_matrix, shape=batch_shape + adj_matrix.shape[-2:]
-            )
-            self.adj_matrix = adj_matrix
-
-        event_shape = (jnp.shape(adj_matrix)[-1],)
-
-        (self.log_sigma,) = promote_shapes(log_sigma, shape=batch_shape)
-
-        super(ICAR_normalized, self).__init__(
-            batch_shape=batch_shape,
-            event_shape=event_shape,
-            validate_args=validate_args,
-        )
-
-        if self._validate_args and (isinstance(self.adj_matrix, np.ndarray) or is_sparse):
-            assert (self.adj_matrix.sum(axis=-1) > 0).all() > 0, (
-                "all sites in adjacency matrix must have neighbours"
-            )
-
-            if self.is_sparse:
-                assert (self.adj_matrix != self.adj_matrix.T).nnz == 0, (
-                    "adjacency matrix must be symmetric"
-                )
-            else:
-                assert np.array_equal(
-                    self.adj_matrix, np.swapaxes(self.adj_matrix, -2, -1)
-                ), "adjacency matrix must be symmetric"
-
-    @property
-    def support(self) -> constraints.Constraint:
-        return logsimplex(-jnp.log(self.dx))
-    
-    def sample(self, key, sample_shape=()):
-        # cannot sample from an improper distribution
-        raise NotImplementedError 
-
-    @validate_sample
-    def log_prob(self, phi):
-
-        adj_matrix = self.adj_matrix
-        conditional_precision = jnp.exp(-2*self.log_sigma)
-        if self.is_sparse:
-            D = np.asarray(adj_matrix.sum(axis=-1))
-            adj_matrix = BCOO.from_scipy_sparse(adj_matrix)
-        else:
-            D = adj_matrix.sum(axis=-1)
-
-        n = D.shape[-1]
-
-        logprec = -2 * (n-1) * self.log_sigma
-
-        logquad = conditional_precision * jnp.sum(
-            phi
-            * (
-                D * phi
-                - (adj_matrix @ phi[..., jnp.newaxis]).squeeze(axis=-1)
-            ),
-            -1,
-        )
-
-        return 0.5 * (logprec - logquad)
-    
-    @staticmethod
-    def infer_shapes(log_sigma, adj_matrix):
-        event_shape = jnp.shape(adj_matrix)[-1]
-        batch_shape = lax.broadcast_shapes(
-            jnp.shape(log_sigma)[:-1], jnp.shape(adj_matrix)[:-2]
-        )
-        return batch_shape, event_shape
+    def log_abs_det_jacobian(self, x, y, intermediates=None):
+        return jnp.zeros(x.shape[:-1])
 
     def tree_flatten(self):
-        data, aux = super().tree_flatten()
-        single_dimension_adj_matrix_data_idx = type(self).gather_pytree_data_fields().index("adj_matrix")
-        single_dimension_adj_matrix_aux_idx = type(self).gather_pytree_aux_fields().index("adj_matrix")
-
-        if not self.is_sparse:
-            aux = list(aux)
-            aux[single_dimension_adj_matrix_aux_idx] = None
-            aux = tuple(aux)
-        else:
-            data = list(data)
-            data[single_dimension_adj_matrix_data_idx] = None
-            data = tuple(data)
-        return data, aux
+        return (), (self.event_shape,)
 
     @classmethod
-    def tree_unflatten(cls, aux_data, params):
-        d = super().tree_unflatten(aux_data, params)
-        if not d.is_sparse:
-            adj_matrix_data_idx = cls.gather_pytree_data_fields().index("adj_matrix")
-            setattr(d, "adj_matrix", params[adj_matrix_data_idx])
-        else:
-            adj_matrix_aux_idx = cls.gather_pytree_aux_fields().index("adj_matrix")
-            setattr(d, "adj_matrix", aux_data[adj_matrix_aux_idx])
-        return d
+    def tree_unflatten(cls, aux_data, children):
+        (event_shape,) = aux_data
+        return cls(event_shape)
+        
+
+class _LogSimplexNDim(constraints.ParameterFreeConstraint):
+    """
+    N-dimensional generalization of _LogSimplex implemented by Jack
+    """
+    event_dim = 1
+
+    def __init__(self, logsumexp: float, event_shape):
+        self.logsumexp = logsumexp
+        self.event_shape = tuple(int(s) for s in event_shape)
+        self._ndim = len(self.event_shape)
+        self._n_tot = int(np.prod(self.event_shape))
+
+    def __call__(self, x):
+        axes = tuple(range(-self._ndim, 0))  
+        x_sum = LSE(x, axis=axes)
+
+        le0 = jnp.all(x <= 0, axis=axes)
+
+        close = (x_sum < self.logsumexp + 1e-6) & (x_sum > self.logsumexp - 1e-6)
+        return le0 & close
+
+    def feasible_like(self, prototype):
+        return jnp.full_like(prototype, self.logsumexp - jnp.log(self._n_tot))
+
+logsimplexNDim = _LogSimplexNDim
+biject_to = numpyro.distributions.transforms.biject_to
+
+@biject_to.register(_LogSimplexNDim)
+def _transform_to_logsimplex_nd(constraint):
+    return ComposeTransform([
+        StickBreakingTransform(),                    
+        ExpTransform().inv,                          
+        AffineTransform(constraint.logsumexp, 1.), 
+        VectorToGridTransform(constraint.event_shape)
+    ])
+
+
+def initialize_ICAR_normalized(dimension):
+    
+    base_ICAR_class = initialize_ICAR(dimension, length_scales=False)
+    
+    class ICAR_normalized(base_ICAR_class):
+        """
+        Sofia note: in Ndim, dx becomes dx*dy*...*dN, so maybe dV is a better name for the parameter?
+        """
+
+        arg_constraints = {
+            "log_sigma": constraints.real,
+            "single_dimension_adj_matrices": constraints.independent(
+                constraints.dependent(is_discrete=False, event_dim=2), 1
+            ),
+            "dx": constraints.positive,
+        }
+        reparametrized_params = ["log_sigma", "single_dimension_adj_matrices", "dx"]
+        pytree_aux_fields = ("is_sparse", "single_dimension_adj_matrices", "dx")
+
+        def __init__(
+            self,
+            log_sigma,
+            single_dimension_adj_matrices,
+            *,
+            is_sparse=False,
+            dx=1.0,
+            validate_args=None,
+        ):
+            self.is_sparse = is_sparse
+            self.dx = dx
+
+            batch_shape = ()
+            (self.log_sigma,) = promote_shapes(log_sigma, shape=batch_shape)
+
+            # store adjacency matrices (sparse or dense)
+            self.single_dimension_adj_matrices = []
+            if self.is_sparse:
+                    for single_dimension_adj_matrix in single_dimension_adj_matrices:
+                        if single_dimension_adj_matrix.ndim != 2:
+                            raise ValueError(
+                                "Currently, we only support 2-dimensional adj_matrix. Please make a feature request",
+                                " if you need higher dimensional adj_matrix.",
+                            )
+                        if not (isinstance(single_dimension_adj_matrix, np.ndarray) or _is_sparse(single_dimension_adj_matrix)):
+                            raise ValueError(
+                                "adj_matrix needs to be a numpy array or a scipy sparse matrix. Please make a feature",
+                                " request if you need to support jax ndarrays.",
+                            )
+                        # TODO: look into future jax sparse csr functionality and other developments
+                        self.single_dimension_adj_matrices.append(_to_sparse(single_dimension_adj_matrix))
+            else:
+                for single_dimension_adj_matrix in single_dimension_adj_matrices:
+                    assert not _is_sparse(single_dimension_adj_matrix), (
+                        "single_dimension_adj_matrix is a sparse matrix so please specify `is_sparse=True`."
+                    )
+                    # TODO: look into static jax ndarray representation
+                    (single_dimension_adj_matrix,) = promote_shapes(
+                        single_dimension_adj_matrix, shape=batch_shape + single_dimension_adj_matrix.shape[-2:]
+                    )
+                    self.single_dimension_adj_matrices.append(single_dimension_adj_matrix)
+
+            event_shape = tuple([jnp.shape(mat)[-1] for mat in self.single_dimension_adj_matrices])
+
+            super(ICAR_normalized, self).__init__(
+                batch_shape=batch_shape,
+                event_shape=event_shape,
+                validate_args=validate_args,
+            )
+
+        @property
+        def support(self) -> constraints.Constraint:
+            return _LogSimplexNDim(logsumexp=-jnp.log(self.dx), event_shape=self.event_shape)
+        
+        def sample(self, key, sample_shape=()):
+            # cannot sample from an improper distribution
+            raise NotImplementedError 
+
+        @validate_sample
+        def log_prob(self, phi):
+            prec = jnp.exp(-2*self.log_sigma)
+            #lams = []
+            prec_mat = []
+            n = 1
+            for ii, single_dimension_adj_matrix in enumerate(self.single_dimension_adj_matrices):
+                if self.is_sparse:
+                    D = np.asarray(single_dimension_adj_matrix.sum(axis=-1)).squeeze(axis=-1)
+                    scaled_single_prec = np.diag(D) - single_dimension_adj_matrix.toarray()
+                else:
+                    D = single_dimension_adj_matrix.sum(axis=-1)# .squeeze(axis=0)
+                    scaled_single_prec = jnp.diag(D) - single_dimension_adj_matrix
+                    
+                n *= D.shape[-1]
+                # TODO: look into sparse eigenvalue methods
+                if isinstance(scaled_single_prec, np.ndarray):
+                    lam = np.linalg.eigvalsh(scaled_single_prec)   
+                    lam[0] = 0. # set to zero, otherwise float precision can allow this to be negative and cause problems   
+                else:
+                    print(jnp.diag(D).shape, single_dimension_adj_matrix.shape)
+                    lam = jnp.linalg.eigvalsh(scaled_single_prec)
+                    lam = lam.at[0].set(0.) # set to zero, otherwise float precision can allow this to be negative and cause problems
+                prec_mat.append(jnp.asarray(scaled_single_prec))
+                #lams.append(prec*lam)
+
+            #ar = reduce(add_outer, lams)
+            #logdet = jnp.sum(jnp.log(ar.at[(0,)*dimension].set(jnp.sum(prec))))
+            logquad = 0.
+            for ii in range(dimension):
+                z = jnp.moveaxis(
+                    jnp.tensordot(prec_mat[ii], jnp.moveaxis(phi,ii,0), axes=(0,0)), # tensordot requires a concrete axis ... can I get this in a jitted fn?
+                    0,ii)
+                step = jnp.tensordot(z, phi, axes=dimension)
+                logquad += step * prec
+
+            logprec = -2.0 * (n - 1) * self.log_sigma
+            return 0.5 * (logprec - logquad)
+            
+
+        @staticmethod
+        def infer_shapes(log_sigma, single_dimension_adj_matrices):
+            event_shape = tuple([jnp.shape(mat)[-1] for mat in single_dimension_adj_matrices])
+            batch_shape = lax.broadcast_shapes(jnp.shape(log_sigma))
+            return batch_shape, event_shape
 
 
 def lower_triangular_sigma_marg_log_prob_and_log_quad(phi, n, single_dimension_adj_matrices):
